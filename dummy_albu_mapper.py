1e3a75393d12a6e439571e501028729725cfcc80
Ilia Denisov <denilv@mail.ru>
2020-02-15 21:53:17 +0300

    [up]: comments are in english now

--- dummy_albu_mapper.py
+++ dummy_albu_mapper.py
@@ -15,16 +15,20 @@ class DummyAlbuMapper:
     To use albumentations:
     1. Create serialized json file with augmentation config. See "sample-detection-albu-config.json"
     2. Define cfg.INPUT.ALBUMENTATIONS variable in detectron config file.
+
+    Also I'm using detectron tools to resize and crop images 
+    because they allow to use:
+    ResizeShortestEdge
+    RandomCrop relative to the original image size
+    
+    To change CROP/RESIZE parameters modify config variables:
+    cfg.INPUT.CROP.[...]
+    cfg.INPUT.[MAX|MIN]_SIZE_[TEST|TRAIN]
     """
     
     def __init__(self, cfg, is_train=True):
         self.aug = self._get_aug(cfg.INPUT.ALBUMENTATIONS)
         self.img_format = cfg.INPUT.FORMAT
-        # для ресайза и кропа использую детектроновские трансформы
-        # т.к. они умеют кропать относительно исходных размеров
-        # и ресайзить по короткой стороне с сохранением сторон
-        # размеры кропов и ресайзов задаются в конфиге:
-        # cfg.INPUT.CROP.[...] и cfg.INPUT.[MAX|MIN]_SIZE_[TEST|TRAIN]
         self.resize_gen = utils.build_transform_gen(cfg, is_train)
         if cfg.INPUT.CROP.ENABLED and is_train:
             self.crop_gen = T.RandomCrop(cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE)
@@ -49,7 +53,7 @@ class DummyAlbuMapper:
         h, w, _ = img.shape
 
         augm_boxes = np.array(augm_annotation['bboxes'], dtype=np.float32)
-        # порой возникали проблемы с тем, что аннотации ббоксов вылазили за пределы картинок
+        # sometimes bbox annotations go beyond image
         augm_boxes[:, :] = augm_boxes[:, :].clip(min=[0, 0, 0, 0], max=[w, h, w, h])
         augm_labels = np.array(augm_annotation['category_id'])
         dataset_dict['annotations'] = [
@@ -63,7 +67,7 @@ class DummyAlbuMapper:
         ]
         
         if self.crop_gen:
-            # кроп картинки средствами детектрона
+            # image crop using detectron tools
             crop_tfm = utils.gen_crop_transform_with_instance(
                 self.crop_gen.get_crop_size(img.shape[:2]),
                 img.shape[:2],
@@ -71,7 +75,7 @@ class DummyAlbuMapper:
             )
             img = crop_tfm.apply_image(img)
 
-        # ресайз средствами детектрона
+        # image resize using detectron tools
         img, transforms = T.apply_transform_gens(self.resize_gen, img)
         if self.crop_gen:
             transforms = crop_tfm + transforms
